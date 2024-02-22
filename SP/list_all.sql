b5207c273d143e5369bb034be9bb5394b8c35bbe
Mariana Bueno <marianabueno@ciandt.com>
2024-02-22 10:14:19 -0300

    Ajustes para calibração de resultado

--- SP/list_all.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[list_all]
-    @lim BIGINT = 9223372036854775807
-AS
-BEGIN
-    SELECT TOP(@lim) *, 5 AS extra_column FROM table_1;
-END
--- SP/output_fixed_size.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[output_fixed_size]
-    @fixed_size VARCHAR(255) OUTPUT
-AS
-BEGIN
-    SET @fixed_size = 'Jambon Beurre'
-END
